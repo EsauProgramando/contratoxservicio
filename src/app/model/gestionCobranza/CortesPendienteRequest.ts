8215a52df348423956dbb7bce9cc6c58c287dfcb
EsauProgramando <eaveintemillap@alumno.unsm.edu.pe>
2025-09-15 22:02:14 -0500

    qwer

--- src/app/model/gestionCobranza/CortesPendienteRequest.ts
+++ src/app/model/gestionCobranza/CortesPendienteRequest.ts
@@ -1,19 +1,23 @@
 export class CortesPendienteRequest {
-  id_cliente: number = 0;
+ id_cliente: number = 0;
   nombre_completo: string = '';
   nrodocident: string = '';
   id_tipo: number = 0;
   desctipo: string = '';
   id_factura: number = 0;
   id_contrato: number = 0;
-  periodo: string = '';
+  periodo_mas_antiguo: string = '';
   saldo: number = 0;
-  fecha_vencimiento: string = '';
+  monto_factura: number = 0;
+  saldo_factura: number = 0;
+  fecha_venc_mas_antigua: string = '';
   dias_mora: number = 0;
-  estado: string = '';
+  estado_factura: string = '';
   id_corte: number = 0;
-  id_estado: number = 0;
+  n_periodos_vencidos: number = 0;
   estadocliente: string = '';
   email: string = '';
   telefono: string = '';
+  deuda_total: number = 0;
+  monto_total: number = 0;
 }
